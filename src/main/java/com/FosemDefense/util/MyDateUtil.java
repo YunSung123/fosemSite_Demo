cc90e8800ed80f3b94066b6d78cbbf3ee8b13891
FOSEM <87117068+YunSung123@users.noreply.github.com>
2026-05-08 14:39:09 +0900

    날짜 / 시간 포맷 변환용 유틸 완성

--- src/main/java/com/FosemDefense/util/MyDateUtil.java
+++ src/main/java/com/FosemDefense/util/MyDateUtil.java
@@ -1,10 +1,11 @@
 package com.FosemDefense.util;
 
-import org.apache.commons.
+import org.apache.commons.lang3.time.DateFormatUtils;
 
 import java.sql.Date;
 import java.sql.Timestamp;
 
+// 날짜/시간 포맷 변환용 유틸 클래스
 public class MyDateUtil {
 
     public static String timestampFormat(Timestamp timestamp){
