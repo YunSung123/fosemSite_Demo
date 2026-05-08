4a5bffdf8ce3bc052b67e0fd5f9aae3f55361460
FOSEM <87117068+YunSung123@users.noreply.github.com>
2026-05-08 14:36:16 +0900

    build.gradle 설정

--- src/main/java/com/FosemDefense/board/Board.java
+++ src/main/java/com/FosemDefense/board/Board.java
@@ -1,11 +1,13 @@
 package com.FosemDefense.board;
 
 import com.FosemDefense.user.User;
+import com.FosemDefense.util.MyDateUtil;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -26,9 +28,15 @@ public class Board {
     private String title;
     private String content;
 
+    @CreationTimestamp
     private LocalDateTime createdAt;
 
+    // 외래 키
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    public String getTime(){
+        return MyDateUtil.timestam
+    }
 }
--- src/main/java/com/FosemDefense/util/MyDateUtil.java
+++ src/main/java/com/FosemDefense/util/MyDateUtil.java
@@ -1,4 +1,14 @@
 package com.FosemDefense.util;
 
+import org.apache.commons.
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
 public class MyDateUtil {
+
+    public static String timestampFormat(Timestamp timestamp){
+        Date currentDate = new Date(timestamp.getTime());
+        return DateFormatUtils.format(currentDate, "yyyy-MM-dd HH:mm");
+    }
 }
